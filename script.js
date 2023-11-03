364ea65aae5cb31f53e820b709b5096a0a8fc61f
ZirhC1 <145370439+ZirhC1@users.noreply.github.com>
2023-11-02 20:32:52 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+// CHECK YOUR UNDERSTANDING1. Create a simple pizza order application:
+//   - Ask the user for the size of pizza and two different toppings
+//   - Output: Your _____  pizza with _____ and _____ will be ready in 20 minutes.
+
+
+alert("hello!");
+alert("I will be asking you some questions");
+var userGuess = prompt("What size of pizza would you like?");
+console.log(`Your answer was ${userGuess}`);
+
+
+var topping1 = prompt("What's the first topping you want on your pizza?");
+console.log(`Your answer was ${topping1}`);
+
+
+var topping2 = prompt("What's the second topping you want on your pizza?");
+console.log(`Your answer was ${topping2}`);
+
+alert(`Your ${userGuess} pizza with ${topping1} and ${topping2} will be ready in 20 minutes...`);
+
+
+alert("Thanks for answering my first question, heres my next one...")
+var course1 = prompt("What is the name of one of the courses you are currently taking?")
+console.log(`Your answer was ${course1}`)
+var mark1 = prompt("What is your current mark in this course?")
+console.log(`your answer was ${mark1}`)
+
+var course2 = prompt("Name a second course.")
+console.log(`Your answer was ${course2}`)
+var mark2 = prompt("What is your current mark in this course?")
+console.log(`your answer was ${mark2}`)
+
+alert(`Course 1: ${course1} with a grade of ${mark1}.`)
+alert(`Course 2: ${course2} with a grade of ${mark2}.`)
+
+
+alert("Ok, this is my last set of questions...")
+var noun = prompt("State a noun")
+console.log(`your answer was ${noun}`)
+
+var pluralnoun = prompt("State a plural noun")
+console.log(`your answer was ${pluralnoun}`)
+
+var verb = prompt("State a verb")
+console.log(`your answer was ${verb}`)
+
+var adjective1 = prompt("State an adjective")
+console.log(`your answer was ${adjective1}`)
+
+var adjective2 = prompt("State another adjective")
+console.log(`your answer was ${adjective2}`)
+
+ alert(`The ${adjective1} ${noun} is known to be able to ${verb} extremely quickly, even faster than ${adjective2} ${pluralnoun}`)
+
+alert("Thanks for answering all my questions!")
+
+// 2. Create a course marks application:
+//   - Ask the user for the names of 2 courses they are taking and the current mark in those courses.
+//   - Output: 
+//         Course 1: _________ with a grade of ______.
+//         Course 2: _________ with a grade of ______.
+
+
+// 3. Create a simple mad lib application.
+//    - Ask the user for a noun, a plural noun, a verb, and 2 adjectives.
+//    - Output:   The ________ ________ is known to be able to _________  extremely quickly, even faster than ________ ________.
+//    - Note: The order of the blanks is:  adjective > noun > verb > adjective > plural noun.
+
+
+// 4. Add all new concepts from this lesson to your CS Notes under the section named INPUT PROCESS OUTPUT.
+
+
